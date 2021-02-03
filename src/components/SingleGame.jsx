bd933e145a1c640d67d51dc3cb9ef82836aab3e7
Valerie TV <vtylskivincent@gmail.com>
2021-02-03 16:19:55 +0100

    added the structure of 1 game to be displayed in a list with Games

--- src/components/SingleGame.jsx
+++ src/components/SingleGame.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SingleGame.css";
 
 function SingleGame(props) {
   const { name, image } = props;
   return (
     <li className="SingleGame">
-      <img src={image} alt={name} />
+      <Link to="/">
+        <img src={image} alt={name} />
+      </Link>
       <h3>{name}</h3>
     </li>
   );
