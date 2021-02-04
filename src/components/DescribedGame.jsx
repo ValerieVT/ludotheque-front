21a88f964c951d9e73a08642a0afb4c4bb862e63
Valerie TV <vtylskivincent@gmail.com>
2021-02-04 14:05:01 +0100

    designed page that describes a game

--- src/components/DescribedGame.jsx
+++ src/components/DescribedGame.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import "./DescribedGame.css";
 
 class DescribedGame extends React.Component {
   constructor(props) {
@@ -28,52 +29,116 @@ class DescribedGame extends React.Component {
   render() {
     const { selectionnedGame } = this.state;
     return (
-      <>
-        <img src="" alt="" />
+      <main className="DescribedGame">
         <h2>{selectionnedGame.name}</h2>
-        <p>{selectionnedGame.summary}</p>
-        <p>Durée (min) : {selectionnedGame.duration_min_in_minuts} min</p>
-        <p>
-          De {selectionnedGame.player_nbmin} à {selectionnedGame.player_nbmax}{" "}
-          joueurs
-        </p>
-        <p>
-          {selectionnedGame.player_agemax !== null
-            ? `A partir de ${selectionnedGame.player_agemin} ans`
-            : ""}
-        </p>
-        <p>
-          {selectionnedGame.player_agemax !== null
-            ? `Déconseillé aux plus de ${selectionnedGame.player_agemax} ans ;)`
-            : ""}
-        </p>
-        <p>{selectionnedGame.collaborative === 1 ? "collaboratif" : ""}</p>
-        <p>{selectionnedGame.asymetric === 1 ? "Jeu asymétrique" : ""}</p>
-        <p>
-          Apprentissage des règles : niveau{" "}
-          {selectionnedGame.gamerule_difficulty}/3
-        </p>
-        <p>
-          {selectionnedGame.generalknowledge !== null
-            ? `Culture générale : ${selectionnedGame.generalknowledge}/3`
-            : ""}
-        </p>
-        <p>
-          {selectionnedGame.chance !== null
-            ? `Chance : ${selectionnedGame.chance}/3`
-            : ""}
-        </p>
-        <p>
-          {selectionnedGame.reflexionRéflexion !== null
-            ? `Réflexion : ${selectionnedGame.reflexion}/3`
-            : ""}
-        </p>
-        <p>
-          {selectionnedGame.skill !== null
-            ? `Adresse : ${selectionnedGame.skill}/3`
-            : ""}
-        </p>
-      </>
+        <p className="summary">{selectionnedGame.summary}</p>
+        <div className="details">
+          <img src="../jeux/a.webp" alt="" />
+          <article>
+            {selectionnedGame.duration_min_in_minuts !== null ? (
+              <p>
+                <img className="picto" src="../logos/duree.svg" alt="" />
+                Durée (min)&nbsp;: {selectionnedGame.duration_min_in_minuts}
+                &nbsp;min
+              </p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.collaborative === 1 ? (
+              <p>
+                <img className="picto" src="../logos/cooperatif.svg" alt="" />
+                Coopératif
+              </p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.asymetric === 1 ? (
+              <p>
+                <img className="picto" src="../logos/asymetrique.svg" alt="" />
+                Asymétrique
+              </p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.player_nbmin !== null &&
+            selectionnedGame.player_nbmax !== null ? (
+              <p className="wider">
+                De {selectionnedGame.player_nbmin} à{" "}
+                {selectionnedGame.player_nbmax} joueurs
+              </p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.player_agemin !== null &&
+            selectionnedGame.player_agemax !== null ? (
+              <p className="wider">
+                {selectionnedGame.player_agemin !== null
+                  ? `Conseillé de ${selectionnedGame.player_agemin} à ${selectionnedGame.player_agemax} ans`
+                  : ""}
+              </p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.player_agemin !== null &&
+            selectionnedGame.player_agemax === null ? (
+              <p className="wider">
+                {selectionnedGame.player_agemin !== null
+                  ? `Peut devenir ennuyeux après ${selectionnedGame.player_agemax} ans`
+                  : ""}
+              </p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.player_agemin === null &&
+            selectionnedGame.player_agemax !== null ? (
+              <p className="wider">
+                {selectionnedGame.player_agemin !== null
+                  ? `A partir de ${selectionnedGame.player_agemin} ans`
+                  : ""}
+              </p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.gamerule_difficulty === "1" ? (
+              <p className="wider">Apprentissage des règles : très facile</p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.gamerule_difficulty === "2" ? (
+              <p className="wider">Apprentissage des règles : accessible</p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.gamerule_difficulty === "3" ? (
+              <p className="wider">Apprentissage des règles : difficile</p>
+            ) : (
+              ""
+            )}
+
+            {selectionnedGame.generalknowledge !== null ? (
+              <p>Culture générale : {selectionnedGame.generalknowledge}/3</p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.chance !== null ? (
+              <p>Chance : {selectionnedGame.chance}/3</p>
+            ) : (
+              ""
+            )}
+            {selectionnedGame.reflexionRéflexion !== null ? (
+              <p>Réflexion : {selectionnedGame.reflexion}/3</p>
+            ) : (
+              ""
+            )}
+
+            {selectionnedGame.skill !== null ? (
+              <p>Adresse : {selectionnedGame.skill}/3</p>
+            ) : (
+              ""
+            )}
+          </article>
+        </div>
+      </main>
     );
   }
 }
