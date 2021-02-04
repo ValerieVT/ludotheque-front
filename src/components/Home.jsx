8ada44b20266544734d0086e9d8dfd40ccf76e9b
Valerie TV <vtylskivincent@gmail.com>
2021-02-04 11:18:20 +0100

    added links with params

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,42 +5,42 @@ export default function Home() {
   return (
     <main className="Home">
       <ul className="choices">
-        <Link to="/jeux">
+        <Link to="/jeux?gamerule_difficulty=1&reflection=1">
           <li>
             <span>J'ai pas DU TOUT envie de réfléchir</span>
           </li>
         </Link>
-        <Link to="/">
+        <Link to="/jeux?reflection=3">
           <li>
             <span>Un jeu pour faire fumer mes méninges&nbsp;!</span>
           </li>
         </Link>
-        <Link to="/">
+        <Link to="/jeux?gamerule_difficulty=1">
           <li>
             <span>Un jeu facile à comprendre</span>
           </li>
         </Link>
-        <Link to="/">
+        <Link to="/jeux?gamerule_difficulty=3">
           <li>
             <span>Je veux me lancer sur une règle du jeu compliquée</span>
           </li>
         </Link>
-        <Link to="/">
+        <Link to="/jeux?duration_min_in_minuts=15">
           <li>
             <span>On joue, mais pas longtemps&nbsp;!</span>
           </li>
         </Link>
-        <Link to="/">
+        <Link to="/jeux?duration_min_in_minuts=90">
           <li>
             <span>Je veux occuper toute ma soirée&nbsp;!</span>
           </li>
         </Link>
-        <Link to="/">
+        <Link to="/jeux?collaborative=1">
           <li>
             <span>Un pour tous, tous pour un&nbsp;!</span>
           </li>
         </Link>
-        <Link to="/">
+        <Link to="/jeux?asymetric=1">
           <li>
             <span>Jeux asymétriques : un rôle différent chacun&nbsp;!</span>
           </li>
@@ -52,7 +52,7 @@ export default function Home() {
         </Link>
       </ul>
       <ul className="half-conteneur">
-        <Link to="/">
+        <Link to="/jeux?">
           <li className="half">
             <span>Un jeu au hasard</span>
           </li>
