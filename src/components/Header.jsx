55934273e75968a4a51990b63da6a9b350a06f04
Valerie TV <vtylskivincent@gmail.com>
2021-02-12 19:21:01 +0100

    added logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,32 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
 
 const Header = () => {
+  const history = useHistory();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URL}auth/logout`,
+        {},
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log("déconnecté");
+      })
+      .then(() => {
+        history.push("/");
+      })
+      .catch((error) => {
+        setErrorMessage(error.response?.data?.error);
+      });
+  };
+
   return (
     <header>
       <h1>
@@ -8,12 +34,15 @@ const Header = () => {
           Ludothèque
         </Link>
       </h1>
-
       <div>
         <Link to="/connexion" title="Connexion">
           Se connecter
         </Link>
+        <form onSubmit={handleSubmit}>
+          <button type="submit">Se déconnecter</button>
+        </form>
       </div>
+      <p className="error-message">{errorMessage}</p>
     </header>
   );
 };
