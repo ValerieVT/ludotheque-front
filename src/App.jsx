e875d46640e1e3c00aa8b7426a473bc104d3c7b0
Valerie TV <vtylskivincent@gmail.com>
2021-04-11 13:14:26 +0200

    Inserted ifConnected state, added it in Login & Header components as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
@@ -11,16 +11,20 @@ import Login from "./components/Login";
 import Admin from "./components/admin/Admin";
 
 export default function App() {
+  const [ifConnected, setIfConnected] = useState(false);
+
   return (
     <div className="App">
-      <Header />
+      <Header ifConnected={ifConnected} setIfConnected={setIfConnected} />
       <article>
         <Switch>
           <Route path="/jeux/:id" component={DescribedGame} />
           <Route exact path="/jeux" component={Games} />
           <Route exact path="/themes/:id/jeux" component={GamesTheme} />
           <Route exact path="/themes" component={Themes} />
-          <Route path="/connexion" component={Login} />
+          <Route path="/connexion">
+            <Login ifConnected={ifConnected} setIfConnected={setIfConnected} />
+          </Route>
           <Route path="/admin" component={Admin} />
           <Route exact to="/" component={Home} />
         </Switch>
